2beea12d35f17cd36b490ec52309e16d8ce7f895
dekuChaurasia <114529313+dekuChaurasia@users.noreply.github.com>
2025-01-04 04:53:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Neocraft",  # Replace with your library's unique name
+    name="Neostate",  # Replace with your library's unique name
     version="0.1.0",  # Start with an initial version
     author="Deku Chaurasia",
     author_email="angryg575@gmail.com",
-    description="Welcome to Neocraft! 🌈 A lightweight and intuitive library for managing shared states in Flet applications. With StateCraft, you can bind widgets to a shared state effortlessly, enabling seamless updates across your UI components with minimal boilerplate.",
+    description="Welcome to Neostate! 🌈 A lightweight and intuitive library for managing shared states in Flet applications. With StateCraft, you can bind widgets to a shared state effortlessly, enabling seamless updates across your UI components with minimal boilerplate.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/dekuChaurasia/Neocraft",  # Add your GitHub repo URL
+    url="https://github.com/dekuChaurasia/Neostate",  # Add your GitHub repo URL
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
